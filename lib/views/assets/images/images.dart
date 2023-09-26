7fef5b6eb1870ed065d8110115b4dcff22dc9eff
Rafa Couto <rafaelcsilva016@outlook.com>
2023-09-25 22:01:28 -0300

    Change app name and add images app

--- lib/views/assets/images/images.dart
+++ lib/views/assets/images/images.dart
@@ -1,5 +1,5 @@
 abstract class ImagesEcokotam {
-  static const String logo = 'lib/views/assets/images/logo.png';
+  static const String logo = 'lib/views/assets/images/icon-android.png';
   static const String google = 'lib/views/assets/images/google.svg';
   static const String artesanato = 'lib/views/assets/images/artesanato.png';
   static const String criancas = 'lib/views/assets/images/criancas.png';
